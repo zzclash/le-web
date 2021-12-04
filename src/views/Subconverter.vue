4831fcdc01bba919b4bf21dbd221310e5ffb192c
zzclash <95514190+zzclash@users.noreply.github.com>
2021-12-04 13:44:44 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://le.gag.workers.dev/sub?" }],
         remoteConfig: [
           {
             label: "universal",
